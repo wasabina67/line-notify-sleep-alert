1d2d4032f8cfdfd5fd37f3e3d6e9b0254a50f67e
wasabina67 <wasabina67@gmail.com>
2024-08-14 20:31:43 +0900

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+source .env
+
+MESSAGE="message"
+IMAGE_FILE="./images/sample.jpg"
+URL="https://notify-api.line.me/api/notify"
+
+curl -X POST -H "Authorization: Bearer $ACCESS_TOKEN" \
+    -F "message=$MESSAGE" \
+    -F "imageFile=@$IMAGE_FILE" \
+    $URL
